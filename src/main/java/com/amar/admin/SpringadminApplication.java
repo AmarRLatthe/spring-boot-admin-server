2797144641253b591116453745fa2564a51be2aa
Amar Latthe <amarlatthe060@gmail.com>
2019-09-12 15:54:42 +0530

    initial_commit

---
+++ src/main/java/com/amar/admin/SpringadminApplication.java
@@ -0,0 +1,16 @@
+package com.amar.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringadminApplication.class, args);
+	}
+
+}
